12d6c00ffcdf419ac845ca789f07c51ab4707eee
Dave Giles <dgiles@saltstack.com>
2022-12-09 16:12:51 -0700

    Update my_script_az.sh

--- my_script_az.sh
+++ my_script_az.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
-yum install -y epel-release vim unzip wget python3
-pip3 install noipy --upgrade
-noipy -u gilesdj@gmail.com -p Snoopy*13 -n saltdemo.sytes.net --provider noip
+HOST=saltdemo.sytes.net
+S1=/usr/local/bin/startup_script.sh
+
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+sudo dnf install -y vim unzip wget python3 python3-pip
+pip3 install noipy
+noipy --store -u gilesdj@gmail.com -p Snoopy*13 -n $HOST --provider noip
+
+cat >$S1 <<EOL
+#!/bin/bash
+noipy -n $HOST --provider noip
+EOL
+
+chmod u+x $S1
+
+cat >/etc/systemd/system/run-at-startup.service <<EOL
+[Unit]
+Description=Run script at startup after all systemd services are loaded
+After=network.target
+
+[Service]
+Type=simple
+User=root
+RemainAfterExit=yes
+ExecStart=$S1
+TimeoutStartSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+chmod 664 /etc/systemd/system/run-at-startup.service
+systemctl daemon-reload
+systemctl enable run-at-startup.service
+systemctl start run-at-startup.service
